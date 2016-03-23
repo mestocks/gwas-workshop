fe5055485c3dee9dbb469323446c915497233cf0
Michael Stocks <ms@mspopgen.com>
2016-03-23 07:11:38 +0000

    Update tutorial.r

--- tutorial.r
+++ tutorial.r
@@ -8,20 +8,20 @@ ruff.clean <- ruff.data[qc$idok, qc$snpok]
 descriptives.trait(ruff.clean)
 descriptives.marker(ruff.clean)
 
-fo.MLR <- mlreg(fo ~ 1, data = ruff.clean, trait = "binomial")
+fo.QT <- qtscore(fo ~ 1, data = ruff.clean, trait = "binomial")
 
-summary(fo.MLR)
-summary(fo.MLR, top = 30)
-sum(fo.MLR[, "P1df"] <= 0.0001)
+summary(fo.QT)
+summary(fo.QT, top = 30)
+sum(fo.QT[, "P1df"] <= 0.0001)
 
-png("fo-mlr.png")
-plot(fo.MLR, col = c("black", "black"), ystart = 1)
+png("fo-qt.png")
+plot(fo.QT, col = c("black", "black"), ystart = 1)
 dev.off()
 
 fo.QT100 <- qtscore(fo ~ 1, data = ruff.clean, trait = "binomial", times = 100)
 summary(fo.QT100)
 
-lambda(fo.MLR)
+lambda(fo.QT)
 gkin <- ibs(ruff.clean)
 mds <- cmdscale(as.dist(0.5 - gkin))
 fo.QTibs <- qtscore(fo ~ mds[, 1] + mds[, 2], data = ruff.clean, trait.type = "binomial")
